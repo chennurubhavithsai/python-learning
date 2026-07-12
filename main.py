8637103c1eb05362da2ed411ce91363422c0a60a
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-12 03:12:04 +0000

    Day 3: Learned if-else statements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-a = 6
-b = 3
+age = int(input("Enter your age: "))
 
-print(a + b)
-print(a * b)
-print(a / b)
-print(a - b)
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+    
