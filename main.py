971f457787933e4daed0e9c79d82c8e99a0d1633
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-14 01:54:40 +0000

    Day 5: Learned for loops

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-age = int(input("Enter your age: "))
-
-if age >= 17 and age <= 25:
-    print("Eligible for college admission")
-else:
-    print("Not in the usual college admission age range")
+for i in range(5):
+    print("hello")
     
