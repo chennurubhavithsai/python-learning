cf87cfb796c6ecbbb31297cc27ae17afeae51e1a
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-16 13:01:23 +0000

    Day 7: Built calculator using functions

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def square(number):
-    return number * number
+def add(a, b):
+    return a + b
 
-print(square(5))
-print(square(8))
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
