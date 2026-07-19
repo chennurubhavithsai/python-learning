2d7ada19fc224f2ccac22e88e4917b8027b31131
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-19 11:20:07 +0000

    Day 10: Built Student Notes Saver

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+name = input("Enter your name: ")
+course = input("Enter your course: ")
+
+file = open("student.txt", "w")
+
+file.write("Name: " + name + "\n")
+file.write("Course: " + course)
+
+file.close()
+
+print("\nData Saved Successfully!")
+
 file = open("student.txt", "r")
 
-content = file.read()
-
-print(content)
+print("\n===== Student Details =====")
+print(file.read())
 
 file.close()
