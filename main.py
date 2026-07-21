904e4eb28cad9ed4ad9ac63c38c4ce1ac5786558
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-21 12:37:05 +0000

    Day 12: Practiced random module

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import math
+import random
 
-number = 25
-
-print("Square Root:", math.sqrt(number))
-print("Power:", math.pow(5, 3))
-print("Ceiling:", math.ceil(4.2))
-print("Floor:", math.floor(4.8))
+print("Random Number:", random.randint(1, 100))
