fdca88fe1fe17ab7700bee4755b69981d00aadae
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-22 11:48:50 +0000

    Day 11: Built safe calculator using exception handling

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 try:
-    number = int(input("Enter a number: "))
-    result = 100 / number
-    print("Result:", result)
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print("Addition:", num1 + num2)
+    print("Subtraction:", num1 - num2)
+    print("Multiplication:", num1 * num2)
+    print("Division:", num1 / num2)
 
 except ValueError:
-    print("Please enter numbers only!")
+    print("Please enter valid numbers.")
 
 except ZeroDivisionError:
-    print("You cannot divide by zero!")
+    print("Cannot divide by zero.")
