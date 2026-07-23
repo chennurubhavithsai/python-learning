ec21f1ecd7850436ef619fe62bbe5f8fc96b6aa8
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-23 12:02:07 +0000

    Day 14: Built Bank Management System

--- main.py
+++ main.py
@@ -6,22 +6,50 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
-        print("₹", amount, "deposited successfully.")
+        print("Deposit Successful!")
 
     def withdraw(self, amount):
         if amount <= self.balance:
             self.balance -= amount
-            print("₹", amount, "withdrawn successfully.")
+            print("Withdrawal Successful!")
         else:
             print("Insufficient Balance!")
 
     def display(self):
-        print("\nCurrent Balance: ₹", self.balance)
+        print("\n===== Account Details =====")
+        print("Account Holder:", self.name)
+        print("Balance: ₹", self.balance)
 
 
-account = BankAccount("Bhavith Sai", 1000)
+name = input("Enter Account Holder Name: ")
+balance = float(input("Enter Initial Balance: "))
 
-account.deposit(500)
-account.withdraw(300)
+account = BankAccount(name, balance)
 
-account.display()
+while True:
+
+    print("\n===== BANK MENU =====")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Check Balance")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        amount = float(input("Enter Deposit Amount: "))
+        account.deposit(amount)
+
+    elif choice == "2":
+        amount = float(input("Enter Withdraw Amount: "))
+        account.withdraw(amount)
+
+    elif choice == "3":
+        account.display()
+
+    elif choice == "4":
+        print("Thank you for using our Bank System!")
+        break
+
+    else:
+        print("Invalid Choice!")
