ced43d329921772a7f7c58412d17efeac86d40c0
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-23 12:00:10 +0000

    Day 14: Added deposit and withdraw methods

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ class BankAccount:
         self.name = name
         self.balance = balance
 
+    def deposit(self, amount):
+        self.balance += amount
+        print("₹", amount, "deposited successfully.")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print("₹", amount, "withdrawn successfully.")
+        else:
+            print("Insufficient Balance!")
+
     def display(self):
-        print("\n===== Account Details =====")
-        print("Name:", self.name)
-        print("Balance: ₹", self.balance)
+        print("\nCurrent Balance: ₹", self.balance)
 
 
 account = BankAccount("Bhavith Sai", 1000)
+
+account.deposit(500)
+account.withdraw(300)
 
 account.display()
