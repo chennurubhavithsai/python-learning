c2d18ddc86914c70db724772352b89e8a0fa23ec
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-24 13:13:56 +0000

    Day 15: Added book management features

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ books = [
     "Machine Learning"
 ]
 
-print("===== Library Books =====")
+new_book = input("Enter a new book: ")
+books.append(new_book)
+
+remove_book = input("Enter a book to remove: ")
+
+if remove_book in books:
+    books.remove(remove_book)
+    print("Book removed successfully!")
+else:
+    print("Book not found!")
+
+print("\n===== Updated Library =====")
 
 for book in books:
     print(book)
