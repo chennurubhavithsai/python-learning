245f85d0dbec280a5f6844f01b417edee752a3f3
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-25 14:10:31 +0000

    Day 16: Added search feature

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 students = []
 
+# Add students
 while True:
     name = input("Enter student name: ")
-    age = input("Enter student age: ")
+    age = input("Enter age: ")
     course = input("Enter course: ")
 
     student = {
@@ -14,11 +15,19 @@ while True:
     students.append(student)
 
     choice = input("Add another student? (yes/no): ")
-
     if choice.lower() != "yes":
         break
 
-print("\n===== Student Records =====")
+# Search student
+search = input("\nEnter student name to search: ")
+
+found = False
 
 for student in students:
-    print(student)
+    if student["name"].lower() == search.lower():
+        print("\nStudent Found!")
+        print(student)
+        found = True
+
+if not found:
+    print("Student not found.")
