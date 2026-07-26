d96ff4504ffc94f80d451659fe5679c549564f8f
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-26 15:52:55 +0000

    Day 17: Added password storage to file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import string
 
 def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
-
     password = ""
 
     for i in range(length):
@@ -11,9 +10,18 @@ def generate_password(length):
 
     return password
 
-length = int(input("Enter password length: "))
+website = input("Enter website name: ")
+username = input("Enter username/email: ")
 
+length = int(input("Enter password length: "))
 password = generate_password(length)
 
-print("\nGenerated Password:")
-print(password)
+print("\nGenerated Password:", password)
+
+with open("passwords.txt", "a") as file:
+    file.write(f"Website: {website}\n")
+    file.write(f"Username: {username}\n")
+    file.write(f"Password: {password}\n")
+    file.write("-----------------------------\n")
+
+print("\n✅ Password saved successfully!")
