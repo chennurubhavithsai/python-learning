96397362810e7ebe21361118a90806b1f7370895
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-07-31 05:51:43 +0000

    Day 22: Added deposit and withdrawal features

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ account = {
 while True:
     print("\n===== BANK MANAGEMENT SYSTEM =====")
     print("1. Create Account")
-    print("2. Check Balance")
-    print("3. Exit")
+    print("2. Deposit Money")
+    print("3. Withdraw Money")
+    print("4. Check Balance")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -20,13 +22,33 @@ while True:
 
     elif choice == "2":
         if account["name"] == "":
-            print("❌ No account found. Please create an account first.")
+            print("❌ Please create an account first.")
         else:
-            print("\n===== ACCOUNT DETAILS =====")
-            print("Account Holder:", account["name"])
-            print("Balance: ₹", account["balance"])
+            amount = float(input("Enter deposit amount: ₹"))
+            account["balance"] += amount
+            print(f"✅ ₹{amount:.2f} deposited successfully!")
 
     elif choice == "3":
+        if account["name"] == "":
+            print("❌ Please create an account first.")
+        else:
+            amount = float(input("Enter withdrawal amount: ₹"))
+
+            if amount <= account["balance"]:
+                account["balance"] -= amount
+                print(f"✅ ₹{amount:.2f} withdrawn successfully!")
+            else:
+                print("❌ Insufficient balance!")
+
+    elif choice == "4":
+        if account["name"] == "":
+            print("❌ No account found.")
+        else:
+            print("\n===== ACCOUNT DETAILS =====")
+            print("Account Holder :", account["name"])
+            print(f"Balance : ₹{account['balance']:.2f}")
+
+    elif choice == "5":
         print("Thank you for using the Bank Management System!")
         break
 
