33ba8af07717662aaa0a18ad0697ed2085904648
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-08-01 12:04:07 +0000

    Day 23: Added search and borrow book features

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ while True:
     print("\n===== LIBRARY MANAGEMENT SYSTEM =====")
     print("1. Add Book")
     print("2. View Books")
-    print("3. Exit")
+    print("3. Search Book")
+    print("4. Borrow Book")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -27,15 +29,48 @@ while True:
             print("\n===== BOOK LIST =====")
             for i, book in enumerate(books, start=1):
                 status = "Available" if book["available"] else "Borrowed"
-
                 print(f"\nBook {i}")
                 print(f"Title : {book['title']}")
                 print(f"Author: {book['author']}")
                 print(f"Status: {status}")
 
     elif choice == "3":
+        search = input("Enter book title: ").lower()
+        found = False
+
+        for book in books:
+            if book["title"].lower() == search:
+                print("\n✅ Book Found")
+                print("Title :", book["title"])
+                print("Author:", book["author"])
+                print("Status:", "Available" if book["available"] else "Borrowed")
+                found = True
+                break
+
+        if not found:
+            print("❌ Book not found.")
+
+    elif choice == "4":
+        borrow = input("Enter book title to borrow: ").lower()
+        found = False
+
+        for book in books:
+            if book["title"].lower() == borrow:
+                found = True
+
+                if book["available"]:
+                    book["available"] = False
+                    print("✅ Book borrowed successfully!")
+                else:
+                    print("❌ Book is already borrowed.")
+                break
+
+        if not found:
+            print("❌ Book not found.")
+
+    elif choice == "5":
         print("Thank you for using Library Management System!")
         break
 
     else:
-        print("❌ Invalid choice.")     
+        print("❌ Invalid choice.")1
