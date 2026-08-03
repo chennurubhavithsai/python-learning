6c66d9161e7ab820ebd2db67ba57cc54bf8d4cdf
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-08-03 12:43:31 +0000

    Day 25: Added inventory deletion, file storage and value calculation

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 products = []
 
+# Load products from file
+try:
+    with open("inventory.txt", "r") as file:
+        for line in file:
+            data = line.strip().split(",")
+
+            if len(data) == 3:
+                products.append({
+                    "name": data[0],
+                    "quantity": int(data[1]),
+                    "price": float(data[2])
+                })
+
+except FileNotFoundError:
+    pass
+
+
 while True:
 
     print("\n===== INVENTORY MANAGEMENT SYSTEM =====")
     print("1. Add Product")
     print("2. View Products")
     print("3. Search Product")
-    print("4. Update Stock")
-    print("5. Exit")
+    print("4. Update Product")
+    print("5. Delete Product")
+    print("6. Calculate Inventory Value")
+    print("7. Save Inventory")
+    print("8. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -15,7 +35,7 @@ while True:
 
         name = input("Enter product name: ")
         quantity = int(input("Enter quantity: "))
-        price = float(input("Enter price per item: ₹"))
+        price = float(input("Enter price: ₹"))
 
         products.append({
             "name": name,
@@ -31,6 +51,7 @@ while True:
             print("No products available.")
 
         else:
+
             print("\n===== PRODUCT LIST =====")
 
             for i, product in enumerate(products, start=1):
@@ -89,6 +110,47 @@ while True:
             print("❌ Product not found.")
 
     elif choice == "5":
+
+        delete = input("Enter product name to delete: ").lower()
+
+        found = False
+
+        for product in products:
+
+            if product["name"].lower() == delete:
+
+                products.remove(product)
+
+                print("✅ Product deleted successfully!")
+
+                found = True
+                break
+
+        if not found:
+            print("❌ Product not found.")
+
+    elif choice == "6":
+
+        total_value = 0
+
+        for product in products:
+            total_value += product["quantity"] * product["price"]
+
+        print(f"\n💰 Total Inventory Value: ₹{total_value:.2f}")
+
+    elif choice == "7":
+
+        with open("inventory.txt", "w") as file:
+
+            for product in products:
+
+                file.write(
+                    f"{product['name']},{product['quantity']},{product['price']}\n"
+                )
+
+        print("✅ Inventory saved successfully!")
+
+    elif choice == "8":
 
         print("Thank you for using Inventory Management System!")
         break
