39dd68544e67a40349a9298cbff4e8c01acbbf95
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-08-04 13:53:00 +0000

    Day 26: Added employee search and update

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ while True:
     print("\n===== EMPLOYEE MANAGEMENT SYSTEM =====")
     print("1. Add Employee")
     print("2. View Employees")
-    print("3. Exit")
+    print("3. Search Employee")
+    print("4. Update Employee")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -42,6 +44,50 @@ while True:
                 print(f"Salary     : ₹{employee['salary']:.2f}")
 
     elif choice == "3":
+
+        search = input("Enter Employee ID: ")
+
+        found = False
+
+        for employee in employees:
+
+            if employee["id"] == search:
+
+                print("\n✅ Employee Found")
+                print("ID         :", employee["id"])
+                print("Name       :", employee["name"])
+                print("Department :", employee["department"])
+                print(f"Salary     : ₹{employee['salary']:.2f}")
+
+                found = True
+                break
+
+        if not found:
+            print("❌ Employee not found.")
+
+    elif choice == "4":
+
+        update = input("Enter Employee ID to update: ")
+
+        found = False
+
+        for employee in employees:
+
+            if employee["id"] == update:
+
+                employee["name"] = input("Enter New Name: ")
+                employee["department"] = input("Enter New Department: ")
+                employee["salary"] = float(input("Enter New Salary: ₹"))
+
+                print("✅ Employee updated successfully!")
+
+                found = True
+                break
+
+        if not found:
+            print("❌ Employee not found.")
+
+    elif choice == "5":
 
         print("Thank you for using Employee Management System!")
         break
