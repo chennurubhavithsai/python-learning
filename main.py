923aa32ea32668ed1f47bf4f1318f843c21c674f
Ch.Bhavith sai <ch.bhavithsai@gmail.com>
2026-08-05 16:17:31 +0000

    Day 27: Added booking search and reservation update

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ while True:
     print("\n===== HOTEL RESERVATION SYSTEM =====")
     print("1. Book Room")
     print("2. View Bookings")
-    print("3. Exit")
+    print("3. Search Booking")
+    print("4. Update Reservation")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -49,6 +51,55 @@ while True:
                 print(f"Total Bill : ₹{booking['total']:.2f}")
 
     elif choice == "3":
+
+        search = input("Enter Booking ID: ")
+
+        found = False
+
+        for booking in bookings:
+
+            if booking["id"] == search:
+
+                print("\n✅ Booking Found")
+                print("Booking ID :", booking["id"])
+                print("Customer   :", booking["customer"])
+                print("Room No.   :", booking["room"])
+                print("Nights     :", booking["nights"])
+                print(f"Price/Night: ₹{booking['price']:.2f}")
+                print(f"Total Bill : ₹{booking['total']:.2f}")
+
+                found = True
+                break
+
+        if not found:
+            print("❌ Booking not found.")
+
+    elif choice == "4":
+
+        update = input("Enter Booking ID to update: ")
+
+        found = False
+
+        for booking in bookings:
+
+            if booking["id"] == update:
+
+                booking["customer"] = input("Enter New Customer Name: ")
+                booking["room"] = input("Enter New Room Number: ")
+                booking["nights"] = int(input("Enter New Number of Nights: "))
+                booking["price"] = float(input("Enter New Price Per Night (₹): "))
+
+                booking["total"] = booking["nights"] * booking["price"]
+
+                print("✅ Reservation updated successfully!")
+
+                found = True
+                break
+
+        if not found:
+            print("❌ Booking not found.")
+
+    elif choice == "5":
 
         print("Thank you for using Hotel Reservation System!")
         break
